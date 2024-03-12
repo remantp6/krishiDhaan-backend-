b66b22ec810661ba1b47b5a7faacff3e382a68b1
subedibimal <bimalsubedi04@gmail.com>
2024-03-12 16:28:34 +0000

    feat: changes in env

--- core/settings.py
+++ core/settings.py
@@ -2,7 +2,9 @@ from pathlib import Path
 import os
 import environ
 
-env = environ.Env()
+env = environ.Env(
+    DEBUG=(bool, True),
+    )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
